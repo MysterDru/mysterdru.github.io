b4fe2a9f95b060c13346096f69272bde1274d949
Drew Frisk <hello@drewfrisk.com>
2024-06-09 21:34:27 -0500

    Initital commit of a blazor solution using a static site generator
    src and dist folders

---
+++ src/mysterdru.github.io/Program.cs
@@ -0,0 +1,38 @@
+using BlazorStatic;
+using mysterdru.github.io.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.WebHost.UseStaticWebAssets();
+
+builder.Services.AddBlazorStaticService(opt =>
+{
+	opt.IgnoredPathsOnContentCopy.Add("app.css"); //pre-build version for tailwind
+
+	opt.OutputFolderPath = "../../dist";
+});
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.UseBlazorStaticGenerator(shutdownApp: !app.Environment.IsDevelopment());
+
+app.Run();
